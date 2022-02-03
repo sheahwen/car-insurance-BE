0f2b05d33187424ce4bf3ebe2f964fbbd6d216a8
sheahwen <sheahwen@gmail.com>
2022-02-03 15:01:51 +0800

    created users, vehicles, licenses models

--- insur_project/insur_project/settings.py
+++ insur_project/insur_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'insur_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'insur_db',
+        'USER': 'insur_admin',
+        'PASSWORD': 'example',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
--- insur_project/main_app/models.py
+++ insur_project/main_app/models.py
@@ -1,3 +1,37 @@
+from django.core.validators import MinValueValidator, RegexValidator, MaxValueValidator
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=30)
+
+
+class Licenses(models.Model):
+    ic_no = models.CharField(max_length=9, primary_key=True, validators=[RegexValidator(regex=r'^[A-Z]\d{7}[A-Z]')])
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255, unique=True)
+    dob = models.DateField()
+    license_no = models.CharField(max_length=9, unique=True, validators=[RegexValidator(regex=r'^[A-Z]\d{7}[A-Z]')])
+    expiry_date = models.DateField()
+    user = models.OneToOneField(Users, on_delete=models.CASCADE)
+
+
+class Vehicles(models.Model):
+
+    today_date = date.today()
+    today_year = today_date.year
+
+    vehicle_no = models.CharField(max_length=9, primary_key=True)
+    vehicle_type = models.CharField(max_length=4)
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    capacity = models.PositiveSmallIntegerField()
+    year_of_registration = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(today_year)])
+    ## may need to check today_year data type
+    coe_expiry_date = models.DateField()
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
